4bcd6beec8f42614293c3889d4ed9b4ef997e6e5
fabiano.antero <fabiano.antero@gmail.com>
2019-04-14 15:00:14 -0300

    Desafio RedeSoft
    Subindo componente NavBar

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <app-menu></app-menu>
+  </div>
+</template>
+
+<script>
+import Menu from './components/navFooter/navBar.vue';
+
+export default {
+  
+  components: {
+    'app-menu' :  Menu
+  }
+}
+</script>
+<style lang="scss">
+@import './assets/styles/variables';
+@import './assets/styles/bootstrap';
+</style>
---
+++ src/components/navFooter/navBar.vue
@@ -0,0 +1,41 @@
+<template>
+
+  <!--Inicio da navBar. ( Class d-lg-none oculta o elemento navBar para
+  telas com resolusão maiores que 999px)-->
+
+  <nav class="d-flex justify-content-center d-lg-none navbar fixed-bottom navbar-light bg-light" >
+    <ul id="nav-root">
+        <li class="nav-item active" >
+          <a class="nav-link" href="#" >
+            <span class="home"></span>
+            <span class="menu-text text-primary">Home</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#" >
+            <span class="promocoes"></span>
+            <span class="menu-text">Promoções</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            <span class="postos"></span>
+            <span class="menu-text">Postos</span>
+          </a>
+        </li>
+        <li class="nav-item" >
+          <a class="nav-link" href="#">
+          <span class="extratos"></span>
+            <span class="menu-text">
+            Extratos</span>
+          </a>
+        </li>
+      </ul>
+  </nav>
+
+</template>
+
+<script>
+</script>
+
+<style src="../../assets/styles/navfooter.css"></style>
